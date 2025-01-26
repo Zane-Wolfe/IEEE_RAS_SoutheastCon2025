87ffc87f0dd74dad5ac5e7fd00b1c816ba5f50f0
Zane Wolfe <85024901+Zane-Wolfe@users.noreply.github.com>
2025-01-26 02:56:09 -0500

    Fixed robot driving, left, right, forward, backwards, and started April Tag code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverLogic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverLogic.java
@@ -6,6 +6,13 @@ public class DriverLogic {
 
     private Main main;
 
+    /**
+     * FLMotor = Front Left Motor
+     * FRMotor = Front Right Motor
+     * BLMotor = Back Left Motor
+     * BRMotor = Back Right Motor
+     */
+
     private DcMotor FLMotor;
     private DcMotor FRMotor;
     private DcMotor BLMotor;
@@ -13,18 +20,69 @@ public class DriverLogic {
 
     public DriverLogic(Main main, DcMotor FLMotor, DcMotor FRMotor, DcMotor BLMotor, DcMotor BRMotor){
         this.main = main;
+        this.FLMotor = FLMotor;
+        this.FRMotor = FRMotor;
+        this.BLMotor = BLMotor;
+        this.BRMotor = BRMotor;
 
-//        waitForStart();
-//        FLMotor.setPower(0.1f);
-//        FRMotor.setPower(0.1f);
-//        BRMotor.setPower(0.1f);
-//        BLMotor.setPower(0.1f);
-//        sleep(2000);
-//        FLMotor.setPower(0);
-//        FRMotor.setPower(0);
-//        BRMotor.setPower(0);
-//        BLMotor.setPower(0);
+        FLMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BLMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+    public void moveLeft(float power){
+        power = Math.abs(power);
+        FLMotor.setPower(-power);
+//        FRMotor.setPower(power*((double) 312 /117));
+        FRMotor.setPower(power);
+        BLMotor.setPower(power*((double) 312 /117));
+        BRMotor.setPower(-power);
+    }
+
+    public void moveRight(float power){
+        power = Math.abs(power);
+        FLMotor.setPower(power);
+//        FRMotor.setPower(-power*((double) 312 /117));
+        FRMotor.setPower(-power);
+        BLMotor.setPower(-power*((double) 312 /117));
+        BRMotor.setPower(power);
+    }
+
+    public void moveForward(float power){
+        power = Math.abs(power);
+        FLMotor.setPower(power);
+//        FRMotor.setPower(power*((double) 312 /117));
+        FRMotor.setPower(power);
+        BLMotor.setPower(power*((double) 312 /117));
+        BRMotor.setPower(power);
+    }
+
+    public void moveBackward(float power){
+        power = Math.abs(power);
+        FLMotor.setPower(-power);
+//        FRMotor.setPower(-power*((double) 312 /117));
+        FRMotor.setPower(-power);
+        BLMotor.setPower(-power*((double) 312 /117));
+        BRMotor.setPower(-power);
+    }
+
+    public void stopMoving(){
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+    }
+
+    public void dumpLog(){
+        main.telemetry.addLine("FLMotor:"+String.valueOf(FLMotor.getCurrentPosition()));
+        main.telemetry.addLine("FRMotor:"+String.valueOf(FRMotor.getCurrentPosition()));
+        main.telemetry.addLine("BLMotor:"+String.valueOf(BLMotor.getCurrentPosition()));
+        main.telemetry.addLine("BRMotor:"+String.valueOf(BRMotor.getCurrentPosition()));
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,27 +1,70 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @Autonomous(name = "main")
 public class Main extends LinearOpMode {
 
-    //   private final DriverLogic driverLogic;
-    private CameraLogic cameraLogic;
+    private DriverLogic driverLogic;
+//    private CameraLogic cameraLogic;
 
     @Override
     public void runOpMode() {
-        cameraLogic = new CameraLogic(this);
-//        driverLogic = setupDriverLogic();
+//        cameraLogic = new CameraLogic(this);
+        driverLogic = setupDriverLogic();
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcamm"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
 
         waitForStart(); // Wait for the game to start
 
         while (opModeIsActive()) {
-            // Process the frame in each loop iteration
-            double driveDirection = cameraLogic.getDriveDirection();
+            if(!tagProcessor.getDetections().isEmpty()){
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                telemetry.addLine("April Tag ID: "+ tag.id);
+            }
 
-            telemetry.addData("Drive Direction Intensity", driveDirection);
+            // Process the frame in each loop iteration
+//            double driveDirection = cameraLogic.getDriveDirection();
+            double driveDirection = 0;
+
+            // 312
+            // 117
+
+            // below 0, go left
+            // above 0, go right
+            float middleThreshold = 0.4f;
+            float power = 0.3f;
+            if(driveDirection > middleThreshold){
+                driverLogic.moveRight(power);
+            }else if(driveDirection < -middleThreshold){
+                driverLogic.moveLeft(power);
+            }else{
+//                driverLogic.moveForward(power);
+            }
+
+//            telemetry.addData("Drive Direction Intensity", driveDirection);
+//            driverLogic.dumpLog();
             telemetry.update();
 
 
@@ -31,14 +74,15 @@ public class Main extends LinearOpMode {
     }
 
     private DriverLogic setupDriverLogic() {
-        DcMotor FLMotor = hardwareMap.get(DcMotor.class, "FrontDriver");
-        DcMotor FRMotor = hardwareMap.get(DcMotor.class, "FrontPassenger");
-        DcMotor BLMotor = hardwareMap.get(DcMotor.class, "BackDriver");
-        DcMotor BRMotor = hardwareMap.get(DcMotor.class, "BackPassenger");
+        DcMotor FLMotor = hardwareMap.get(DcMotor.class, "frontdriver");
+        FLMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        DcMotor FRMotor = hardwareMap.get(DcMotor.class, "frontpassenger");
+        FRMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        DcMotor BLMotor = hardwareMap.get(DcMotor.class, "reardriver");
+        BLMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        DcMotor BRMotor = hardwareMap.get(DcMotor.class, "rearpassenger");
+        BRMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         return new DriverLogic(this, FLMotor, FRMotor, BLMotor, BRMotor);
     }
-
-
-
 
 }
